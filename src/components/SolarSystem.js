a41298defc25e21325338aca8fd4e01504ca6a20
Elivelton <eliveltonmn@outlook.com>
2022-08-18 16:42:03 -0300

    Css Basico

--- src/components/SolarSystem.js
+++ src/components/SolarSystem.js
@@ -8,11 +8,16 @@ class SolarSystem extends React.Component {
     return (
       <div data-testid="solar-system">
         <Title headline="Planetas" />
-        {planets.map((planet) => (<PlanetCard
-          key={ planet.name }
-          planetName={ planet.name }
-          planetImage={ planet.image }
-        />))}
+        <div className="container-planets">
+          {planets.map((planet) => (
+            <PlanetCard
+              className="container"
+              key={ planet.name }
+              planetName={ planet.name }
+              planetImage={ planet.image }
+            />
+          ))}
+        </div>
       </div>
     );
   }
